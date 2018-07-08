26c4fc13c8efcc3244205c0117ebb25fd0f2c119
David Komer <dakom@users.noreply.github.com>
2018-07-09 00:08:29 +0300

    Update World-Update.ts

--- src/world/World-Update.ts
+++ src/world/World-Update.ts
@@ -2,11 +2,9 @@ import { updateBox } from "./box/Box-Update";
 import { updateCamera } from "./camera/Camera-Update";
 import {Renderer, Controller, World} from "../types/Types";
 
-export const updateWorld = (renderer: Renderer) => (controller: Controller) => (dt: number) => (oldWorld: World): World => {
-  const world = Object.assign({}, oldWorld);
-  
-  world.camera = updateCamera (controller) (renderer.getViewport()) (world.camera);
-  world.box = updateBox (controller) (world.camera ) (dt) (world.box);
+export const updateWorld = (renderer: Renderer) => (controller: Controller) => (dt: number) => (world: World): World => {
+  const camera = updateCamera (controller) (renderer.getViewport()) (world.camera);
+  const box = updateBox (controller) (camera) (dt) (world.box);
 
-  return world;
+  return Object.assign({}, world, {camera, box});
 }
